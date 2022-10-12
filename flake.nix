c71eb5c3c14280f059a6c12780af6fe62181ef91
hhefesto <dherrera@fpcomplete.com>
2022-10-12 10:14:37 -0500

    squash

--- flake.nix
+++ flake.nix
@@ -3,35 +3,71 @@
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        overlays = [ haskellNix.overlay
-          (final: prev: {
-            nix-demo =
-              final.haskell-nix.project {
-                src = ./.;
-                shell.buildInputs = with pkgs; [
-                  stack
-                  nixpkgs-fmt
-                  postgresql
-                  nixUnstable
-                  # inputs.deploy-rs.defaultPackage.x86_64-linux
-                ];
-                shell.additional = hsPkgs: with hsPkgs; [ Cabal ];
-              };
-          })
-        ];
-        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-        flake = pkgs.nix-demo.flake {};
-      in flake // {
-        packages = flake.packages // {
-          default = flake.packages."nix-demo:exe:nix-demo";
-        };
-        apps = flake.apps // { default = flake.apps."nix-demo:exe:nix-demo"; };
+  inputs.flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus";
+  inputs.deploy-rs = {
+    url = "github:serokell/deploy-rs";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-utils, haskellNix, flake-utils-plus, deploy-rs }:
+    let
+      nixosModules = flake-utils-plus.lib.exportModules (
+        nixpkgs.lib.mapAttrsToList (name: value: ./nixosModules/${name}) (builtins.readDir ./nixosModules)
+      );
+      # overlays = [ haskellNix.overlay
+      #   (final: prev: {
+      #     nix-demo =
+      #       final.haskell-nix.project {
+      #         src = ./.;
+      #         shell.buildInputs = with pkgs; [
+      #           stack
+      #           nixpkgs-fmt
+      #           postgresql
+      #           nixUnstable
+      #           # inputs.deploy-rs.defaultPackage.x86_64-linux
+      #         ];
+      #         shell.additional = hsPkgs: with hsPkgs; [ Cabal ];
+      #       };
+      #   })
+      # ];
+      # pkgs = import nixpkgs { system = "x86_64-linux"; inherit overlays; inherit (haskellNix) config; };
+      # flake = pkgs.nix-demo.flake {};
+      flake-deploy-rs = flake-utils-plus.lib.mkFlake {
+        inherit self inputs nixosModules;
 
-        legacyPackages = pkgs;
-      });
+        hosts = {
+          hetzner.modules = with nixosModules; [
+            nix-demo
+            common
+            admin
+            hardware-hetzner
+          ];
+        };
+
+        deploy.nodes = {
+          my-node = {
+            hostname = "135.181.45.205";
+            fastConnection = false;
+            profiles = {
+              my-profile = {
+                sshUser = "admin";
+                path =
+                  inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.hetzner;
+                user = "root";
+              };
+            };
+          };
+        };
+      };
+
+    # in flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: flake // {
+    #     packages = flake.packages // {
+    #       default = flake.packages."nix-demo:exe:nix-demo";
+    #     };
+    #     apps = flake.apps // { default = flake.apps."nix-demo:exe:nix-demo"; };
+
+    #     legacyPackages = pkgs;
+    #   }) // flake-deploy-rs;
+    in flake-deploy-rs;
 
   # --- Flake Local Nix Configuration ----------------------------
   nixConfig = {
