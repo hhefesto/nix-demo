be4a46a09a76177fc68292bb5502d8f08576b5e9
hhefesto <dherrera@fpcomplete.com>
2022-10-12 09:52:00 -0500

    squash

--- src/Model.hs
+++ src/Model.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE EmptyDataDecls             #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GADTs                      #-}
